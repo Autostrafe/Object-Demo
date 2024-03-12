2a74d0f1e7a45f233886164f72a7710505e7af79
Autostrafe <Levibautista360@gmail.com>
2024-03-12 12:10:24 -0600

    New Ball

--- sketch.js
+++ sketch.js
@@ -5,11 +5,52 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let ballsArray = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  createBall();
+ 
 }
 
 function draw() {
-  background(220);
+  background(40);
+
+  fill(255,0,0);
+ 
+
+  for (let i = 0; i < ballsArray.length; i++){
+    circle(ballsArray[i].x, ballsArray[i].y, ballsArray[i].diameter);
+    ballsArray[i].x += ballsArray[i].dx; // X-Rate of change
+    ballsArray[i].y += ballsArray[i].dy; // Y-Rate of Change
+
+  }
+
+
+}
+
+
+
+
+function createBall() { 
+
+  // Creates a temp ball obj
+  let newBall = {
+    x: random(width),
+    y: random(height),
+    diameter: random(25, 100),
+    dx: random(-5, 5),
+    dy: random(-5, 5),
+  };
+
+  if (newBall.x === windowWidth)
+
+  // Copying ball obj into the array
+  ballsArray.push(newBall);
+
+}
+
+function mousePressed(){
+  createBall()
+  createBall()
 }
